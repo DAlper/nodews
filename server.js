08db46d279790b60c0477c4b47f8275ad6a16a7c
DAlper <da@feedgee.com>
2017-03-23 15:26:03 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+res.writeHead(200, {"Content-Type": "text/html"});
+res.write('<!DOCTYPE html>'+
+'<html>'+
+' <head>'+
+' <meta charset="utf-8" />'+
+' <title>My Node.js page!</title>'+
+' </head>'+ 
+' <body>'+
+' <p>Here is a paragraph of <strong>HTML</strong>!</p>'+
+' </body>'+
+'</html>');
+res.end();
+});
+server.listen(process.env.PORT || 3000);
+
+
+var GMT_UNIXTIME = function getCurrentTimeUTC()
+{
+    //RETURN:
+    //      = number of milliseconds between current UTC time and midnight of January 1, 1970
+    var tmLoc = new Date();
+    //The offset is in minutes -- convert it to ms
+    return tmLoc.getTime() + tmLoc.getTimezoneOffset() * 60000;
+};
+
+process.stdout.write('Unix Time: ' + GMT_UNIXTIME + ' Right?\n');
